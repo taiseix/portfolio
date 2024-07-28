855ee1e9c745390e13f43de335bec50760eceb1a
taiseix <5tainewgphone2@gmail.com>
2024-07-29 00:48:19 +0900

    syuusei7

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 //logoの表示
 $(window).on('load',function(){
-    $("#splash_buttom").click(function() {
+    $("#splash_logo").click(function() {
         $("#splash").delay(500).fadeOut('slow');
         $("#splash_logo").delay(500).fadeOut('slow');
   });
